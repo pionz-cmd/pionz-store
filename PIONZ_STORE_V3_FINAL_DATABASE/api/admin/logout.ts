2f5664b2b8ce4a8b4fbe94ae8415c8a8825a5128
pionz-cmd <jastebbambang12@gmail.com>
2026-09-05 18:15:34 +0700

    Delete PIONZ_STORE_V3_FINAL_DATABASE directory

--- PIONZ_STORE_V3_FINAL_DATABASE/api/admin/logout.ts
+++
@@ -1,8 +0,0 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { clearAdminSessionCookie } from '../_adminSession';
-
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  if (req.method !== 'POST') return res.status(405).json({ error: 'Method Not Allowed' });
-  clearAdminSessionCookie(res);
-  return res.status(200).json({ ok: true });
-}
